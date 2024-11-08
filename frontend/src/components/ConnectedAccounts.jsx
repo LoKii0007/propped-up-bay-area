9f4114a24131cfb551368fc26fa9517b8f9ac709
loki <lokeshyadav1098@gmail.com>
2024-11-08 12:30:30 +0530

    '

--- frontend/src/components/ConnectedAccounts.jsx
+++ frontend/src/components/ConnectedAccounts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext";
-import LinkGoogleAccount from "./LinkGoogleAccount";
+import LinkGoogleAccount from "../auth/LinkGoogleAccount";
+
 
 const ConnectedAccounts = () => {
   const { currentUser } = useAuth();
