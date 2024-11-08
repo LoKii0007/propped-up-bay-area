d81ac1ef70a412c4d78f25d243469b381b57f406
loki <lokeshyadav1098@gmail.com>
2024-11-08 11:20:28 +0530

    '

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 const Routes = require('./routes/routes')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const compression = require('compression')
 
 const app = express()
 const PORT = 5000
@@ -17,9 +16,6 @@ app.use(cors({
     origin: ['http://localhost:5173', 'https://propped-up-bay-area.vercel.app'], 
     credentials: true,
 }))
-
-//? compression
-// app.use(compression())
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
