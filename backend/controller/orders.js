384735db4cb89843dc69ac9f5639312fa6b1d790
loki <lokeshyadav1098@gmail.com>
2024-12-08 11:26:36 +0530

    '

--- backend/controller/orders.js
+++ backend/controller/orders.js
@@ -107,6 +107,7 @@ const completeOpenHouseOrder = async (orderId, session) => {
     console.log("orderId",orderId);
 
     const id = new mongoose.Types.ObjectId(orderId)
+    console.log("id",id);
     const order = await openHouseSchema.findOne({ _id: id });
 
     if (!order) {
