950ce421e7611d3598fc3376c2a2bb2c0f35d9b6
loki <lokeshyadav1098@gmail.com>
2024-12-23 15:18:13 +0530

    responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,27 +15,27 @@ export const Navbar = ({ setActiveView, activeView }) => {
 
     return (
         <>
-            <nav className='grid grid-cols-4 p-5 fixed bottom-0 z-50 w-full bg-white md:hidden navbar '>
-                <div className='flex items-center justify-center gap-2'>
-                    <button onClick={() => handleView('Dashboard')} className={` custom-transition flex flex-col items-center justify-center gap-1 p-2 rounded-lg ${activeView === 'dashboard' ? 'bg-[#638856] text-white font-semibold' : 'text-[#737791] font-medium'}`}>
+            <nav className='grid grid-cols-4 p-3 fixed bottom-0 text-xs z-50 w-full bg-white md:hidden navbar '>
+                <div className='flex items-center justify-center gap-2 w-full '>
+                    <button onClick={() => handleView('Dashboard')} className={` custom-transition flex flex-col items-center justify-center gap-1 p-2 rounded-lg w-full ${activeView === 'dashboard' ? 'bg-[#638856] text-white font-semibold' : 'text-[#737791] font-medium'}`}>
                         <img src='/svg/dashboard.svg' alt="logo" />
                         <span>Home</span>
                     </button>
                 </div>
-                <div className='flex items-center justify-center gap-2'>
-                    <button onClick={() => handleView('Order')} className={` custom-transition flex flex-col items-center justify-center gap-1 p-2 rounded-lg  ${activeView === 'order' ? 'bg-[#638856] text-white font-semibold' : 'text-[#737791] font-medium'}`}>
+                <div className='flex items-center justify-center gap-2 w-full'>
+                    <button onClick={() => handleView('Order')} className={` custom-transition flex flex-col items-center justify-center gap-1 p-2 rounded-lg w-full  ${activeView === 'order' ? 'bg-[#638856] text-white font-semibold' : 'text-[#737791] font-medium'}`}>
                         <img src='/svg/order.svg' alt="logo" />
                         <span>Order</span>
                     </button>
                 </div>
-                <div className='flex items-center justify-center gap-2'>
-                    <button onClick={() => handleView('Removal')} className={` custom-transition flex flex-col items-center justify-center gap-1 p-2 rounded-lg  ${activeView === 'removal' ? 'bg-[#638856] text-white font-semibold' : 'text-[#737791] font-medium'}`}>
+                <div className='flex items-center justify-center gap-2 w-full'>
+                    <button onClick={() => handleView('Removal')} className={` custom-transition flex flex-col items-center justify-center gap-1 p-2 rounded-lg w-full  ${activeView === 'removal' ? 'bg-[#638856] text-white font-semibold' : 'text-[#737791] font-medium'}`}>
                         <img src='/svg/removal.svg' alt="logo" />
                         <span>Removal</span>
                     </button>
                 </div>
-                <div className='flex items-center justify-center gap-2'>
-                    <button onClick={() => handleView('Profile')} className={` custom-transition flex flex-col items-center justify-center gap-1 p-2 rounded-lg  ${activeView === 'profile' ? 'bg-[#638856] text-white font-semibold' : 'text-[#737791] font-medium'}`}>
+                <div className='flex items-center justify-center gap-2 w-full'>
+                    <button onClick={() => handleView('Profile')} className={` custom-transition flex flex-col items-center justify-center gap-1 p-2 rounded-lg w-full  ${activeView === 'profile' ? 'bg-[#638856] text-white font-semibold' : 'text-[#737791] font-medium'}`}>
                         <img src='/svg/settings.svg' alt="logo" />
                         <span>Profile</span>
                     </button>
